d67018c4619c35a20286420dba9bf54639cb058d
uAliFurkanY <alifurkanyildiz@gmail.com>
2021-01-20 23:06:58 +0300

    basic bot connection

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+const mineflayer = require("mineflayer");
+const bot = mineflayer.createBot({
+	host: process.env.HOST,
+	username: process.env.USER,
+	password: process.env.PASS,
+});
