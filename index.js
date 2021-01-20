104d3bf2cc311bb5e85ef3640c086593ac298063
uAliFurkanY <alifurkanyildiz@gmail.com>
2021-01-20 23:59:58 +0300

    - Chat relay - Added module - Update default env

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
+const { Webhook, MessageBuilder } = require("discord-webhook-node");
+let hook = new Webhook(process.env.HOOK);
 const mineflayer = require("mineflayer");
 const bot = mineflayer.createBot({
 	host: process.env.HOST,
 	username: process.env.USER,
 	password: process.env.PASS,
 });
+const rl = require("readline").createInterface({ input: process.stdin, output: process.stdout });
 bot.once("login", () => console.log("LOGGED IN"));
 bot.once("spawn", onSpawn);
 bot.once("end", onEnd);
@@ -14,6 +17,14 @@ console.log("BOT STARTED");
 
 function onSpawn() {
 	console.log("BOT SPAWNED");
+	rl.on("line", bot.chat);
+	bot.on("chat", (u, s) => {
+		console.log(u, s);
+		if (hook) {
+			hook.setUsername(u);
+			hook.send(s.replace(/(@everyone)|(@here)|(<@.{0,1}[0-9]{18}>)/g, "[PING]"));
+		}
+	});
 }
 function onEnd() {
 	console.log("BOT END");
